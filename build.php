1b6acd655670eacfd8c1de0324472f28062b21e8
Dzmitry Vensko <github.com@belbit.net>
2015-09-25 02:09:53 +0200

    Updated builder

--- build.php
+++ build.php
@@ -11,9 +11,23 @@ $phar = new Phar($destFile);
 $phar->addFile(__DIR__.'/mtags.php', 'mtags.php');
 $phar->setStub($phar->createDefaultStub('mtags.php'));
 
+// for completeness
+$phar->addFile(__FILE__, basename(__FILE__));
+$phar->addFile(__DIR__.'/composer.json', 'composer.json');
+
 foreach (glob(__DIR__.'/src/*.php') as $file) {
 	$phar->addFile($file, 'src/'.basename($file));
 }
+
+$phar->addFile(__DIR__.'/vendor/autoload.php', 'vendor/autoload.php');
+
+foreach (glob(__DIR__.'/vendor/composer/*.php') as $file) {
+	$phar->addFile($file, 'vendor/composer/'.basename($file));
+}
+
+$phar->addFile(__DIR__.'/vendor/pwfisher/command-line-php/CommandLine.php', 'vendor/pwfisher/command-line-php/CommandLine.php');
+
+$getid3dir = 'vendor/james-heinrich/getid3/getid3';
 
 $getid3modules = [
 	'getid3',
@@ -25,12 +39,12 @@ $getid3modules = [
 	'module.audio-video.quicktime'
 ];
 
-foreach (glob(__DIR__.'/getID3/getid3/*.php') as $file) {
+foreach (glob(__DIR__.'/'.$getid3dir.'/*.php') as $file) {
 	$basename = basename($file);
 
 	foreach ($getid3modules as $module) {
 		if (strpos($basename, $module) === 0) {
-			$phar->addFile($file, 'getID3/getid3/'.$basename);
+			$phar->addFile($file, $getid3dir.'/'.$basename);
 			break;
 		}
 	}
